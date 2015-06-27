ada0328752ea605f04c497e24e6ebe26a555a108
lucas <lucasiba777@gmail.com>
2015-06-27 14:45:55 -0300

    First commit by Lucas

---
+++ iCodename/iCodename/iCodename/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iCodename
+//
+//  Created by Claudio Ibañez on 27/6/15.
+//  Copyright (c) 2015 utilmobile. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+ 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ iCodename/iCodename/iCodename/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iCodename
+//
+//  Created by Claudio Ibañez on 27/6/15.
+//  Copyright (c) 2015 utilmobile. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
