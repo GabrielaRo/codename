28d8f4e44a99f2a5ce2cb16857723605b0eb8880
grogdj <grogdj@gmail.com>
2015-08-21 16:17:12 +0100

    - updating ranges

--- codename-services/src/main/java/org/codename/services/endpoints/impl/DistanceRange.java
+++ codename-services/src/main/java/org/codename/services/endpoints/impl/DistanceRange.java
@@ -10,11 +10,15 @@ package org.codename.services.endpoints.impl;
  * @author grogdj
  */
 public enum DistanceRange {
-    _1KM(0.0, 1.0, "Within 1 KM"),
-    _3KM(1.0, 3.0, "Within 3 KM"),
-    _10KM(3.0, 10.0, "Within 10 KM"),
-    _50KM(10.0, 50.0, "Within 50 KM"),
-    _WORLD(50.0, 10000.0, "Far Away"),
+    _1KM(0.0, 1.0, "Within 1km"),
+    _3KM(1.0, 3.0, "Within 3km"),
+    _5KM(3.0, 5.0, "Within 5km"),
+    _10KM(5.0, 10.0, "Within 10km"),
+    _20KM(10.0, 20.0, "Within 20km"),
+    _30KM(15.0, 30.0, "Within 30km"),
+    _50KM(30.0, 50.0, "Within 50km"),
+    _100KM(50.0, 100.0, "Within 100km"),
+    _WORLD(100.0, 50000.0, "Over 100km away"),
     _ALL(-1.0, -1.0, "The World" );
 
     private Double offsetRange;
