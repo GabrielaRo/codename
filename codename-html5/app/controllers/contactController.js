2f08d4624856f7cee25c4dc6c6d1b34347e3dc25
grogdj <salaboy@gmail.com>
2015-09-03 23:45:04 +0100

    - user feedback changes :)

--- codename-html5/app/controllers/contactController.js
+++ codename-html5/app/controllers/contactController.js
@@ -1,10 +1,24 @@
 (function () {
-    var contactController = function ($scope, $rootScope, $users, $auth, appConstants) {
+    var contactController = function ($scope, $rootScope, $users, $auth, appConstants, $contact) {
        $scope.pageClass = "contact";
        $( window ).scrollTop( 0 );
+       
+        $scope.sendContactForm = function (email, name, subject, text, type) {
+
+            $contact.sendMessage(email, name, subject, text, type).success(function (data) {
+                $scope.contactStatus = true;
+                $rootScope.$broadcast("quickNotification", "<i class='fa fa-check'></i> Message Sent", 'success');
+            }).error(function (data) {
+                $rootScope.$broadcast("quickNotification", "<i class='fa fa-exclamation-triangle'></i> Something failed: " + data, 'error');
+                console.log("Error : " + data + "!");
+
+            });
+
+        }
+       
     };
 
-    contactController.$inject = ['$scope', '$rootScope', '$users', '$auth', 'appConstants'];
+    contactController.$inject = ['$scope', '$rootScope', '$users', '$auth', 'appConstants', '$contact'];
     angular.module("codename").controller("contactController", contactController);
 
 }());
--- codename-html5/app/controllers/feedbackController.js
+++ codename-html5/app/controllers/feedbackController.js
@@ -1,10 +1,25 @@
 (function () {
-    var feedbackController = function ($scope, $rootScope, $users, $auth, appConstants) {
+    var feedbackController = function ($scope, $rootScope, $users, $auth, appConstants, $contact) {
        $scope.pageClass = "feedback";
        $( window ).scrollTop( 0 );
+       
+        $scope.sendContactForm = function (email, name, subject, text, type) {
+
+            $contact.sendMessage(email, name, subject, text, type).success(function (data) {
+                $scope.feedbackContactStatus = true;
+                $rootScope.$broadcast("quickNotification", "<i class='fa fa-check'></i> Message Sent", 'success');
+            }).error(function (data) {
+                $rootScope.$broadcast("quickNotification", "<i class='fa fa-exclamation-triangle'></i> Something failed: " + data, 'error');
+                console.log("Error : " + data + "!");
+
+            });
+
+        }
+       
     };
 
-    feedbackController.$inject = ['$scope', '$rootScope', '$users', '$auth', 'appConstants'];
+    
+    feedbackController.$inject = ['$scope', '$rootScope', '$users', '$auth', 'appConstants', '$contact'];
     angular.module("codename").controller("feedbackController", feedbackController);
 
 }());
