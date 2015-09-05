c476224b1a92516a27ff3a3fe078f3dcf50d95c7
esala <ezequiel.salatino@gmail.com>
2015-09-05 17:32:08 +0200

    Settings section and password form

--- codename-html5/app/controllers/settingsController.js
+++ codename-html5/app/controllers/settingsController.js
@@ -1,24 +1,43 @@
 (function () {
     var settingsController = function ($scope, $rootScope, $users, $auth, $trace, appConstants) {
-        $scope.pageClass = "contact";
+        $scope.pageClass = "settings";
         $(window).scrollTop(0);
         $scope.sharedLocations = [];
+        $scope.formStatus;
+        $scope.currentSection = "password";
+        
+        
         $scope.updatePassword = function (oldPassword, newPassword, newPasswordConfirm) {
-            if (newPassword == newPasswordConfirm) {
-                $users.updatePassword(oldPassword, newPassword).success(function (data) {
+            if($scope.passwordForm.$valid){
+              
+                if (newPassword == newPasswordConfirm) {
+                    $users.updatePassword(oldPassword, newPassword).success(function (data) {
 
-                    $rootScope.$broadcast("quickNotification", "<i class='fa fa-check'></i> Password Updated", 'success');
-                }).error(function (data) {
-                    $rootScope.$broadcast("quickNotification", "<i class='fa fa-exclamation-triangle'></i> Something failed: " + data, 'error');
-                    console.log("Error : " + data + "!");
+                        $rootScope.$broadcast("quickNotification", "<i class='fa fa-check'></i> Password Updated", 'success');
+                        $scope.formStatus = "";
+                  
+                    }).error(function (data) {
+                        $scope.formStatus = "Incorrect Password"
+                        //$rootScope.$broadcast("quickNotification", "<i class='fa fa-exclamation-triangle'></i> Something failed: " + data, 'error');
+                        console.log("Error : " + data + "!");
 
-                });
-            } else {
-                $rootScope.$broadcast("quickNotification", "<i class='fa fa-exclamation-triangle'></i> Passwords doesn't match! ", 'error');
+                    });
+                } else {
+                    $scope.formStatus = "Passwords doesn't match."
+                   // $rootScope.$broadcast("quickNotification", "<i class='fa fa-exclamation-triangle'></i> Passwords doesn't match! ", 'error');
 
+                }
+            }else {
+                console.log($scope.passwordForm.$valid);
+                $scope.formStatus = "Fill all fields";
             }
 
         }
+        
+        $scope.setSection = function(section){
+            $scope.currentSection = section;
+        }
+        
         $scope.loadSharedLocations = function () {
             $trace.getSharedLocations().success(function (data) {
 
